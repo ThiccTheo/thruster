7249a91a803372c774e0749365e0dad1f715c397
Theo Lee <th3o1237@gmail.com>
2024-11-17 18:46:56 -0500

    gaining some momentum on the project again

--- src/main.rs
+++ src/main.rs
@@ -5,13 +5,36 @@ mod query;
 
 use {
     eframe::{self, NativeOptions},
+    egui::IconData,
     engine::Engine,
+    image::{GenericImageView, ImageReader},
+    std::path::PathBuf,
 };
 
 fn main() {
     eframe::run_native(
         "thRUSTer",
-        NativeOptions::default(),
+        {
+            let mut options = NativeOptions::default();
+            options.viewport = options.viewport.with_icon({
+                let image = ImageReader::open(
+                    PathBuf::from(format!("{}\\assets\\icon.png", env!("CARGO_MANIFEST_DIR")))
+                        .as_path(),
+                )
+                .unwrap()
+                .decode()
+                .unwrap();
+
+                let (width, height) = image.dimensions();
+
+                IconData {
+                    width,
+                    height,
+                    rgba: image.into_rgba8().into_vec(),
+                }
+            });
+            options
+        },
         Box::new(|creation_ctx| Ok(Box::new(Engine::from(creation_ctx)))),
     )
     .unwrap();
